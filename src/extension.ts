920b1db15562848781066a3ed4bd5b51791b66db
Robert Gill <envman@gmail.com>
2021-01-07 20:38:12 +0000

    add treeview

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,21 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Dependency, DepNodeProvider } from './nodeDependencies';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+		// Samples of `window.registerTreeDataProvider`
+		// vscode.workspace.workspaceFolders[0]
+		const nodeDependenciesProvider = new DepNodeProvider(vscode.workspace.rootPath || '');
+		vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
+		vscode.commands.registerCommand('nodeDependencies.refreshEntry', () => nodeDependenciesProvider.refresh());
+		vscode.commands.registerCommand('extension.openPackageOnNpm', moduleName => vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)));
+		vscode.commands.registerCommand('nodeDependencies.addEntry', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
+		vscode.commands.registerCommand('nodeDependencies.editEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
+		vscode.commands.registerCommand('nodeDependencies.deleteEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
